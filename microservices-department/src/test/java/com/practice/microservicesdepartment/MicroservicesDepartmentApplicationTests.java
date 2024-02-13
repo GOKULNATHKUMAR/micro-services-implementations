b961254036974fbdd58a4e84d43cb0eeb702b777
EQ30221 <gokulnath.kumar@bell.ca>
2024-02-13 16:14:04 +0530

    department services microservices base project

---
+++ microservices-department/src/test/java/com/practice/microservicesdepartment/MicroservicesDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.microservicesdepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
